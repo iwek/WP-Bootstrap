1bec15a621eb4593fe4f87a21610a9c477cfa73c
Iwo Kadziela <iwokadziela@gmail.com>
2012-01-17 11:18:56 -0500

    footer change

--- bootstrap/footer.php
+++ bootstrap/footer.php
@@ -3,7 +3,6 @@
 
       <footer>
         <p class="left">Copyright &copy; <a href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a></p>
-        <p class="right"><a href="http://risingtrends.com/blog/bootstrap-wordpress-theme/">WP-Bootstrap</a> theme by <a href="http://risingtrends.com">RisingTrends</a><span id="footer-pad"></p>
       </footer>
 
 
